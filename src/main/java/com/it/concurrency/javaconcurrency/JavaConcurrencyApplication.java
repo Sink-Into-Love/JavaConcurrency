bc7bebad0163d0e3749061053815e1f44603be0f
zph <1305353123@qq.com>
2021-08-19 17:37:39 +0800

    Initial commit

---
+++ src/main/java/com/it/concurrency/javaconcurrency/JavaConcurrencyApplication.java
@@ -0,0 +1,16 @@
+package com.it.concurrency.javaconcurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zph
+ */
+@SpringBootApplication
+public class JavaConcurrencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaConcurrencyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/it/concurrency/javaconcurrency/JavaConcurrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.it.concurrency.javaconcurrency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaConcurrencyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
